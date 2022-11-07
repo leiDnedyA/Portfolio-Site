c98976ee7ad8098c881e1c09de59b5e6d620a0a3
Ayden <aydendiel@gmail.com>
2022-11-07 10:54:54 -0500

    Added footer w/ contact info

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,23 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home';
 import Contact from './Pages/Contact';
 import Projects from './Pages/Projects';
+import SocialLinks from './Elements/SocialLinks';
 
 function App() {
   return (
     <div className="App">
       <Navbar links={[
-        { label: 'Home', destination: '/' }, 
-        { label: 'Projects', destination: '/projects' }, 
+        { label: 'Home', destination: '/' },
+        { label: 'Projects', destination: '/projects' },
         { label: 'Contact', destination: '/contact' }]} />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="projects" element={<Projects />} />
         <Route path="contact" element={<Contact />} />
       </Routes>
+      <div className="footer">
+         <a href="mailto: aydendiel@gmail.com">aydendiel@gmail.com</a> <SocialLinks />
+      </div>
     </div>
   );
 }
