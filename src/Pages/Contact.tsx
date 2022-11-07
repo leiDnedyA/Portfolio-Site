bb12896fdce6f7ac2d60c14e567aa6743588c554
Ayden <aydendiel@gmail.com>
2022-11-07 10:22:07 -0500

    Added some basic contact to Contact page

--- src/Pages/Contact.tsx
+++ src/Pages/Contact.tsx
@@ -1,5 +1,10 @@
+import AutoTypingHeader from "../Elements/AutoTypingHeader"
+import SocialLinks from "../Elements/SocialLinks"
 
 
 export default (): JSX.Element => {
-    return <div>Contact route...</div>
+    return <div>
+        <AutoTypingHeader text="Contact Me"/>
+        <SocialLinks />
+    </div>
 }
