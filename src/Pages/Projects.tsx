f33d86ac365bc15f08e123ea01a01604c96be817
Ayden <aydendiel@gmail.com>
2022-11-07 11:03:07 -0500

    Added content and styling to Projects page

--- src/Pages/Projects.tsx
+++ src/Pages/Projects.tsx
@@ -3,6 +3,10 @@ import AutoTypingHeader from "../Elements/AutoTypingHeader"
 
 export default (): JSX.Element =>{
     return <div className="projects">
-        <AutoTypingHeader text="My Projects"/>
+        <div className="projects-header">
+        <AutoTypingHeader className="projects-header-header" text="My Projects"/>
+        <p className="projects-header-description">Psssttt... Check out <a className="no-bold" href="">my GitHub profile</a>! Filler text filler text filler text</p>
+        </div>
+        
     </div>
 }
