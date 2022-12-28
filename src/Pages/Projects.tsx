30f866b4b5481f3bb0c265d3c857e3ca0d83c545
Ayden Diel <aydendiel@gmail.com>
2022-12-28 13:01:23 -0500

    updated projects page

--- src/Pages/Projects.tsx
+++ src/Pages/Projects.tsx
@@ -17,7 +17,6 @@ export default (props: Props): JSX.Element => {
     return <div className="Projects">
         <div className="projects-header">
             <AutoTypingHeader className="projects-header-header" text="My Projects" />
-            <p className="projects-header-description">Psssttt... Check out <a className="no-bold" href="">my GitHub profile</a>! Here are a few of the projects I've worked on over the past couple years.</p>
         </div>
         <div className="projects-container">
             {props.projectsList.map((p, i) => (
