6e3420ab8a86e59b02fbdc8f306896617817621b
Ayden <aydendiel@gmail.com>
2023-01-02 15:13:39 -0500

    Changed app.js for debugging heroku

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT | 3000;
 
-app.use(express.static('build'))
+// app.use(express.static('build'))
+
+app.get('/', (req, res)=>{
+    res.send('Hello world!');
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
