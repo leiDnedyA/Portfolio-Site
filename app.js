014c6eb8da0356b9c538a85e22813063dd3c2874
Ayden <aydendiel@gmail.com>
2023-01-02 15:35:05 -0500

    Fixed port issue for heroku

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require("dotenv").config()
 const express = require('express')
 const app = express()
-const port = process.env.PORT | 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static('build'))
 app.get('*', (req, res)=>{
