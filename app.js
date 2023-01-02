f0ff6664807981cfcbb2a1f5c7f2d4348c762d32
Ayden <aydendiel@gmail.com>
2023-01-02 15:28:58 -0500

    setup for heroku

--- app.js
+++ app.js
@@ -3,10 +3,9 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT | 3000;
 
-// app.use(express.static('build'))
-
-app.get('/', (req, res)=>{
-    res.send('Hello world!');
+app.use(express.static('build'))
+app.get('*', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 })
 
 app.listen(port, () => {
