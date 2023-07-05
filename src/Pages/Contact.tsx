62a8979ffc1c57a1b18f33bd7f71ff61688295aa
Ayden Diel <aydendiel@gmail.com>
2023-07-05 16:50:04 -0400

    tweaked contact page text

--- src/Pages/Contact.tsx
+++ src/Pages/Contact.tsx
@@ -6,7 +6,7 @@ import ResumeButton from '../Elements/ResumeButton';
 export default (): JSX.Element => {
     return <div className="Contact">
         <AutoTypingHeader text="Contact Me" />
-        <p className="contact-page-body">Let's get in touch! I'm actively looking for part-time remote work and an internship for summer 2024. If you're a recruiter, especially one looking to fill such positions, I'd love to chat!</p>
+        <p className="contact-page-body">Let's get in touch! I'm actively looking for part-time remote work and an internship for summer 2024. If you're a recruiter/hiring manager I'd love to chat!</p>
         <ContactInfo/>
         <ResumeButton/>
     </div>
