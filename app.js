61eba82fbf15dd24acddb5d1f327c51431cad583
Ayden Diel <aydendiel@gmail.com>
2023-07-14 15:34:07 -0400

    added thrift-router files

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-require("dotenv").config()
-const express = require('express')
-const app = express()
+import express from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
+import { placesAPIRouter } from './thrift_router/api/placesAPI';
+
 const port = process.env.PORT || 3000;
-const path = require('path');
+const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', (req, res)=>{
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 })
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
 })
+
+app.use(placesAPIRouter);
---
+++ thrift_router/api/placesAPI.js
@@ -0,0 +1,49 @@
+import dotenv from 'dotenv';
+import { Router } from 'express';
+
+dotenv.config();
+
+/**
+ * Makes a request to a places API to find thrift shops
+ * within a given radius (in meters)
+ * 
+ * Note: currently using the geoapify places API but that
+ * may change in the near future
+ * 
+ * @param {number[]} latLng lat and long of search point
+ * @param {number} radius radius in meters of search
+ * @returns {{name: string, position: number[]}[]} objects representing found theift shops
+ * 
+ */
+async function thriftsWithinRadius(latLng, radius) {
+    return fetch(`https://api.geoapify.com/v2/places?categories=commercial.second_hand,commercial.antiques&filter=circle:${latLng[1]},${latLng[0]},${radius}&bias=proximity:${latLng[1]},${latLng[0]}&limit=20&apiKey=${process.env.GEOAPIFY_KEY}`)
+        .then(res => res.json())
+        .then(result => {
+            return result.features.reduce((accumulator, curr) => {
+                const currProps = curr.properties;
+                const currPlace = {
+                    name: currProps.name,
+                    position: [currProps.lat, currProps.lon]
+                };
+                if (currProps.hasOwnProperty("address_line1") && currProps.hasOwnProperty("address_line2")) {
+                    currPlace.address = {
+                        "line1": currProps.address_line1,
+                        "line2": currProps.address_line2
+                    }
+                }
+                accumulator.push(currPlace);
+                return accumulator;
+            }, []);
+        })
+}
+
+const placesAPIRouter = new Router()
+
+placesAPIRouter.use('/api/searchCircle/pos/:lat,:lon/rad/:radius', (req, res) => {
+    thriftsWithinRadius([req.params.lat, req.params.lon], req.params.radius)
+    .then(result => {
+        res.send(result);
+    });
+});
+
+export default placesAPIRouter;
