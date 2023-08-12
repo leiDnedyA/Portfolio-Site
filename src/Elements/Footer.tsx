58b7b1eeeadc454507f19c23622f257d4e4ae0d2
leiDnedyA <aydendiel@gmail.com>
2023-08-12 13:42:35 -0400

    Updated resume and skills grid

--- src/Elements/Footer.tsx
+++ src/Elements/Footer.tsx
@@ -1,5 +1,6 @@
+import { FaGithub } from 'react-icons/fa'
 import './Footer.css'
 
 export default () => <div className="Footer">
-    <p>Designed and Built by Ayden Diel.</p>
+    <p>Designed and Built by <a href="https://github.com/leiDnedyA/">Ayden Diel <FaGithub/></a></p>
 </div>
