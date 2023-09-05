4b4baa02324a0e91c59c020ff46bfd2ea3f577f2
leiDnedyA <aydendiel@gmail.com>
2023-09-05 11:30:51 -0400

    added configRouter to app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 import express from 'express';
-import dotenv from 'dotenv';
+import dotenv, { config } from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
 dotenv.config();
 import { placesAPIRouter } from './thrift_router/api/placesAPI.mjs';
+import configRouter from './routes/config.js'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -15,6 +15,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.use(placesAPIRouter);
+app.use(configRouter);
 
 app.use('/thrift-router', express.static('thrift_router/dist'))
 
